c60e9d59a5705ac3fd020a655ccfaaa1dd972661
Jupeeter8 <aswalanirudh@outlook.in>
2022-12-16 11:35:13 +0530

    init

---
+++ app/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI, responses
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/", response_class=responses.RedirectResponse)
+async def main():
+    return "/docs"
