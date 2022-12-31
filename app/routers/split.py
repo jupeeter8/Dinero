75c5aa7b2c30272f3f8992a1255f1e575179e363
Jupeeter8 <aswalanirudh@outlook.in>
2022-12-31 18:36:15 +0530

    Feature: Added funtionality to split between two users

--- app/routers/split.py
+++ app/routers/split.py
@@ -10,7 +10,7 @@ from ..oAuth2 import get_current_user
 
 router = APIRouter(tags=["Balance and settle"])
 
-# TODO Check the two user are friends
+
 @router.post("/record/split")
 async def split(
     split_detail: SplitDetails,
@@ -22,14 +22,14 @@ async def split(
     validate_user(split_detail.owed_by, db)
 
     # Check if user is not requesting to add split data for someone else
-    if (
-        not split_detail.paid_by == current_user_ID
-        and not split_detail.owed_by == current_user_ID
-    ):
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail=f"User Can only add balance for themselves",
-        )
+    created_by = {split_detail.p_username: split_detail.paid_by}
+    if not split_detail.paid_by == current_user_ID:
+        if not split_detail.owed_by == current_user_ID:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="User can not spli for other users",
+            )
+        created_by = {split_detail.o_username: split_detail.owed_by}
 
     # Check if the user is trying to add split with themselve
     if split_detail.paid_by == split_detail.owed_by:
@@ -38,23 +38,36 @@ async def split(
         )
 
     # Check both users are firends
-    isfriend = db.query(models.Friends).filter(
-        (
+    # Just use two different queries
+
+    # Maybe move this logic to servies?
+
+    isfriend = (
+        db.query(models.Friends)
+        .filter(
             models.Friends.sender == split_detail.paid_by,
             models.Friends.reciver == split_detail.owed_by,
         )
-        or (
-            models.Friends.sender == split_detail.owed_by,
-            models.Friends.reciver == split_detail.paid_by,
-        )
+        .first()
     )
 
-    print(isfriend)
-
     if not isfriend:
+        isfriend = (
+            db.query(models.Friends)
+            .filter(
+                models.Friends.sender == split_detail.owed_by,
+                models.Friends.reciver == split_detail.paid_by,
+            )
+            .first()
+        )
+        if not isfriend:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED, detail="Users are not friends"
+            )
+
+    if not isfriend.status:
         raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail=f"User is not friends with the other user",
+            status_code=status.HTTP_403_FORBIDDEN, detail=f"Users are not friends"
         )
 
     # Validate amounts and convert to paise
@@ -62,4 +75,10 @@ async def split(
         split_detail.paid_amount, split_detail.owed_amount
     )
 
-    return None
+    data = models.Split(**split_detail.dict())
+    data.created_by = created_by
+
+    db.add(data)
+    db.commit()
+    db.refresh(data)
+    return data
