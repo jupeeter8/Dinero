8495579065759962ef0988bd2a71273b2d73a9b9
Jupeeter8 <aswalanirudh@outlook.in>
2022-12-31 09:16:43 +0530

    added get username service

--- app/routers/split.py
+++ app/routers/split.py
@@ -3,21 +3,23 @@ from sqlalchemy.orm import Session
 
 from app.schema import SplitDetails
 from app.service.splitService import verify_split
-from app.service.usersService import validate_user
+from app.service.usersService import get_username, validate_user
 from .. import models
 from ..database import get_db
 from ..oAuth2 import get_current_user
 
 router = APIRouter(tags=["Balance and settle"])
 
-# TODO Added Created by column in Split table
+# TODO Check the two user are friends
 @router.post("/record/split")
 async def split(
     split_detail: SplitDetails,
     current_user_ID: int = Depends(get_current_user),
     db: Session = Depends(get_db),
 ):
-    # data = split_detail.dict()
+    # Validate both user_id
+    validate_user(split_detail.paid_by, db)
+    validate_user(split_detail.owed_by, db)
 
     # Check if user is not requesting to add split data for someone else
     if (
@@ -35,12 +37,29 @@ async def split(
             status_code=status.HTTP_400_BAD_REQUEST, detail="User can not owe themselve"
         )
 
-    # Validate both user_id
-    validate_user(split_detail.paid_by, db)
-    validate_user(split_detail.owed_by, db)
+    # Check both users are firends
+    isfriend = db.query(models.Friends).filter(
+        (
+            models.Friends.sender == split_detail.paid_by,
+            models.Friends.reciver == split_detail.owed_by,
+        )
+        or (
+            models.Friends.sender == split_detail.owed_by,
+            models.Friends.reciver == split_detail.paid_by,
+        )
+    )
+
+    print(isfriend)
+
+    if not isfriend:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail=f"User is not friends with the other user",
+        )
 
     # Validate amounts and convert to paise
     split_detail.paid_amount, split_detail.owed_amount = verify_split(
         split_detail.paid_amount, split_detail.owed_amount
     )
+
     return None
