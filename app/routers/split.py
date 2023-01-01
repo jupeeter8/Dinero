0b43f475f7850a91209c7e167583dd822eb1d46f
Jupeeter8 <aswalanirudh@outlook.in>
2023-01-01 09:54:37 +0530

    Fixes #9

--- app/routers/split.py
+++ app/routers/split.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, HTTPException, Depends, status
 from sqlalchemy.orm import Session
 
 from app.schema import SplitDetails
+from app.service.friendsService import check_friend_status
 from app.service.splitService import verify_split
 from app.service.usersService import get_username, validate_user
 from .. import models
@@ -17,11 +18,16 @@ async def split(
     current_user_ID: int = Depends(get_current_user),
     db: Session = Depends(get_db),
 ):
-    # Validate both user_id
-    validate_user(split_detail.paid_by, db)
-    validate_user(split_detail.owed_by, db)
+
+    # Check if the user is trying to add split with themselve
+
+    if split_detail.paid_by == split_detail.owed_by:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="User can not owe themselve"
+        )
 
     # Check if user is not requesting to add split data for someone else
+
     created_by = {split_detail.p_username: split_detail.paid_by}
     if not split_detail.paid_by == current_user_ID:
         if not split_detail.owed_by == current_user_ID:
@@ -31,45 +37,18 @@ async def split(
             )
         created_by = {split_detail.o_username: split_detail.owed_by}
 
-    # Check if the user is trying to add split with themselve
-    if split_detail.paid_by == split_detail.owed_by:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST, detail="User can not owe themselve"
-        )
+    # Validate both user_id
+    if split_detail.paid_by == current_user_ID:
+        validate_user(split_detail.owed_by, db)
+    else:
+        validate_user(split_detail.paid_by, db)
 
     # Check both users are firends
-    # Just use two different queries
 
-    # Maybe move this logic to servies?
-
-    isfriend = (
-        db.query(models.Friends)
-        .filter(
-            models.Friends.sender == split_detail.paid_by,
-            models.Friends.reciver == split_detail.owed_by,
-        )
-        .first()
-    )
-
-    if not isfriend:
-        isfriend = (
-            db.query(models.Friends)
-            .filter(
-                models.Friends.sender == split_detail.owed_by,
-                models.Friends.reciver == split_detail.paid_by,
-            )
-            .first()
-        )
-        if not isfriend:
-            raise HTTPException(
-                status_code=status.HTTP_401_UNAUTHORIZED, detail="Users are not friends"
-            )
-
-    if not isfriend.status:
+    if not check_friend_status(split_detail.paid_by, split_detail.owed_by, db):
         raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN, detail=f"Users are not friends"
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Users are not friends"
         )
-
     # Validate amounts and convert to paise
     split_detail.paid_amount, split_detail.owed_amount = verify_split(
         split_detail.paid_amount, split_detail.owed_amount
@@ -77,6 +56,8 @@ async def split(
 
     data = models.Split(**split_detail.dict())
     data.created_by = created_by
+
+    print(created_by)
 
     db.add(data)
     db.commit()
--- app/service/friendsService.py
+++ app/service/friendsService.py
@@ -1,4 +1,8 @@
+from fastapi import HTTPException, status
+from sqlalchemy.orm import Session
 import time
+
+from app import models
 
 
 def generate_code():
@@ -15,3 +19,50 @@ def generate_code():
     time_int = int(time_str[:-6:-1])
 
     return time_int
+
+
+def check_friend_status(a: int, b: int, db: Session):
+    # isfriend = (
+    #     db.query(models.Friends)
+    #     .filter(
+    #         models.Friends.sender == a,
+    #         models.Friends.reciver == b,
+    #     )
+    #     .first()
+    # )
+
+    # if not isfriend:
+    #     isfriend = (
+    #         db.query(models.Friends)
+    #         .filter(
+    #             models.Friends.sender == b,
+    #             models.Friends.reciver == a,
+    #         )
+    #         .first()
+    #     )
+    #     if not isfriend:
+    #         return False
+
+    if (
+        isfriend := (
+            db.query(models.Friends)
+            .filter(
+                models.Friends.sender == a,
+                models.Friends.reciver == b,
+            )
+            .first()
+        )
+    ) is None:
+        if (
+            isfriend := (
+                db.query(models.Friends)
+                .filter(
+                    models.Friends.sender == b,
+                    models.Friends.reciver == a,
+                )
+                .first()
+            )
+        ) is None:
+            return False
+
+    return True if isfriend.status else False
